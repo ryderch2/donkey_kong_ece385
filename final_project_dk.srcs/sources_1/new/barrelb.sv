2b4f4d7002af4f0dba64f55a67ab8b31289d5c85
Ryder Heit <ryderch2@illinois.edu>
2023-12-06 16:04:47 -0600

    bro idek

---
+++ final_project_dk.srcs/sources_1/new/barrelb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/06/2023 12:34:40 PM
+// Design Name: 
+// Module Name: barrelb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module barrelb(
+
+    );
+endmodule
---
+++ final_project_dk.srcs/sources_1/new/barrelr.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/06/2023 12:33:20 PM
+// Design Name: 
+// Module Name: barrelr
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module barrelr(
+
+    );
+endmodule
